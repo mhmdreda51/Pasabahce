6eb49de5e296c9c2a8dc965f0057d68a4d3df730
Muhammed Reda <60475330+mhmdreda51@users.noreply.github.com>
2022-07-25 17:16:09 +0200

    settings working

---
+++ lib/view/Drawer/Componants/drawer_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrawerBody extends StatelessWidget {
+  const DrawerBody({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/view/Settings/Componants/user_profile_image.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserProfileImage extends StatelessWidget {
+  const UserProfileImage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
